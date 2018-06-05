6b51558ca7e841ca8e6f45951f055dbd3d2db22b
Andre Steenveld <andre.steenveld@gmail.com>
2018-06-05 12:49:04 +0200

    Well... it is running and breaking!

---
+++ test/process-request/get-query.js
@@ -0,0 +1,38 @@
+import URI from "urijs";
+import test from "ava";
+import defaults from "../../src/defaults";
+import { build_sort, build_fields, build_matches, build_limit_and_offset } from "../../src/process-request/get-query";
+
+function output_equals( assert, value_func, $args, expected ){
+
+    return assert.deepEqual( value_func( ...$args ), expected );
+
+}
+
+const context = {
+    meta : { 
+        
+        request : { 
+            
+            url : ( new URI( ) )
+                .protocol( "http" )
+                .host( "example.localhost" )
+                .directory( "type" )
+                .query({
+                    sort : "+accending,neutral,-decending"
+
+
+                })
+                .toString( )
+        } 
+    
+    }
+}
+
+test( "build_sort", output_equals, defaults :: build_sort, [{ }], {
+
+    neutral   : true,
+    accending : true,
+    decending : false 
+
+});
