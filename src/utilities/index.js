00fecff2a1be110469f39f27ac08fe5d35c280fd
Andre Steenveld <andre.steenveld@gmail.com>
2018-06-05 11:11:44 +0200

    Add the utilities we needed

---
+++ src/utilities/index.js
@@ -0,0 +1,47 @@
+import oo_pointer from "json8-pointer";
+
+export function noop( ){ }
+
+export function identity( v ){ return v; }
+
+export function value( value ){
+    
+    return "undefined" === typeof this ? value : this;
+    
+}
+
+export function get( path, default_value ){
+    
+    return oo_pointer.find( this, path ) :: value( default_value );
+    
+}
+
+export function empty( ){
+
+    return Object.create( null );
+
+}
+
+export function flatten( target = this, array_or_object ){
+
+    return target.concat( array_or_object );
+
+}
+
+export function to_object( target, [ key, value ] ){
+
+    return Object.assign( target, { [ key ] : value } );
+
+}
+
+export function truthy( v ){
+
+    return true === !!v;
+
+}
+
+export function falsey( v ){
+
+    return false === !!v;
+
+}
