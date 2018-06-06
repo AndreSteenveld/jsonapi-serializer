7a84d031373686b0ec4d0d82c9fe09616c25ca64
Andre Steenveld <andre.steenveld@gmail.com>
2018-06-06 13:50:27 +0200

    Make the output a little prettier

---
+++ test/process-request.js
@@ -0,0 +1,12 @@
+import test from "ava";
+import defaults from "../src/defaults"; 
+import { get_query, get_type, get_ids, get_meta, process_request } from "../src/process-request";
+
+function is_function( assert, v ){ return assert.true( "function" === typeof v ); }
+
+test( "get_query( ... ) is a function", is_function, get_query );
+test( "get_type( ... ) is a function", is_function, get_type );
+test( "get_ids( ... ) is a function", is_function, get_ids );
+test( "get_meta( ... ) is a function", is_function, get_meta );
+test( "process_request( ... ) is a function", is_function, process_request );
+
