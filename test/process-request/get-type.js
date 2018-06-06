7a05b41a91f06343596755e3df061e307ef70ef2
Andre Steenveld <andre.steenveld@gmail.com>
2018-06-06 14:25:08 +0200

    Simple test showing that we can unpack the type from a url

--- test/process-request/get-type.js
+++ test/process-request/get-type.js
@@ -1,3 +1,7 @@
 import test from "ava";
+import { output_equals, context } from "./_helpers";
 
-test.todo( "get_type" );
+import defaults from "../../src/defaults";
+import { get_type } from "../../src/process-request";
+
+test( "returns the type from unpacking the url", output_equals, defaults :: get_type, [ null, context ], "type" );
