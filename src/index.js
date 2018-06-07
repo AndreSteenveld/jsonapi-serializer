76bb85543a3bb4ae7913853c62862fd58d69588a
Andre Steenveld <andre.steenveld@gmail.com>
2018-06-07 10:20:31 +0200

    Use the factory for transformers in the constructor of serializer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import transformalizer from "transformalizer";
-
 import process_request from "./process-request";
 import process_response from "./process-response";
 import parse_payload from "./parse-payload";
+
+import transform from "./schema-to-transformer";
+
 
 export { default as defaults } from "./defaults";
 
@@ -11,12 +12,14 @@ export process_request, process_response, parse_payload;
 export default ( Serializer ) => class extends Serializer {
     
     static mediaType = "application/vnd.api+json";
-    
+   
+    tranformer = null;
+
     constructor( ...$args ){
         
         super( ...$args );  
         
-        
+        this.tranformer = this.schema :: transform({ });
         
     }
     
